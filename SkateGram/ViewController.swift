2d0fda58aace3042eb78c770d1a520f7b911aafa
brokemo <trane0909@bk.ru>
2021-12-21 10:16:15 +0300

    Initial Commit

---
+++ SkateGram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SkateGram
+//
+//  Created by Никита Мошенцев on 21.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
